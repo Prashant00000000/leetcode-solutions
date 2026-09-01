3e0eef313c9cbbabccaedb4ee67c5c2dd9cc49e0
Prashant Upadhyay <prashant.24scse1011301@galgotiasuniversity.ac.in>
2026-09-01 09:46:57 +0530

    Time: 16 ms (3.56%), Space: 26.3 MB (7.73%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+         int l = 0;
+         int h = n-1;
+         while(l<h){
+            int mid = l + (h-l)/2;
+            if(mid%2==0){
+                // even index hai it means 1st occurence
+                if( nums[mid]==nums[mid+1]){
+                    l = mid+2;
+                }
+                else{
+                    h = mid;
+                }
+            }
+            else{
+                // mid odd hai
+                if(nums[mid]==nums[mid-1]){
+                    l = mid+1;
+                }
+                else{
+                    h = mid;
+                }
+            }
+            
+         }
+         return nums[l];
+    }
+};
