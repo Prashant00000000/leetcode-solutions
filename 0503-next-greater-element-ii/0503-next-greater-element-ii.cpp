8a7abab3a0b2c13f30998ab425c95aea855a0726
Prashant Upadhyay <prashant.24scse1011301@galgotiasuniversity.ac.in>
2026-09-21 22:56:01 +0530

    Time: 94 ms (7.11%), Space: 28.5 MB (37.92%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,34 +4,21 @@ public:
         vector<int> ans;
         int n = nums.size();
 
-        if(n == 1){
-            ans.push_back(-1);
-            return ans;
-        }
+        for(int i = 0; i < n; i++) {
 
-        for(int i = 0; i < n; i++){
+            int j;
 
-            int j; 
-            for(j = i + 1; j < n; j++){
-                if(nums[i] < nums[j]){
-                    ans.push_back(nums[j]);
+            for(j = i + 1; j < i + n; j++) {
+
+                int index = j % n; 
+
+                if(nums[i] < nums[index]) {
+                    ans.push_back(nums[index]);
                     break;
                 }
             }
-            if(j == n){
-                for(int m = 0; m < i; m++){
-                    if(nums[i] < nums[m]){
-                        ans.push_back(nums[m]);
-                        break;
-                    }
-
-                    if(m == i - 1){
-                        ans.push_back(-1);
-                    }
-                }
-                if(i == 0){
-                    ans.push_back(-1);
-                }
+            if(j == i + n) {
+                ans.push_back(-1);
             }
         }
 
