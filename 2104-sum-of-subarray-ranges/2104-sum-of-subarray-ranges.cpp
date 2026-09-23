d5f9170c2826c124080b95f22926b243fd8ad1e8
Prashant Upadhyay <prashant.24scse1011301@galgotiasuniversity.ac.in>
2026-09-23 11:23:08 +0530

    Time: 2 ms (79.35%), Space: 16.2 MB (7.77%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,17 +1,137 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        long long sum = 0;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            int mini = INT_MAX;
-            int maxi = INT_MIN;
-            for(int j=i; j<n; j++){
-                maxi = max(maxi,nums[j]);
-                mini = min(mini,nums[j]);
-                sum = sum + (maxi - mini);
+
+    // Next Greater
+    vector<int> nextgreater_Ele(vector<int>& arr){
+        stack<int> st;
+        vector<int> ans;
+
+        for(int i = arr.size()-1; i >= 0; i--){
+            while(!st.empty() && arr[st.top()] < arr[i]){
+                st.pop();
             }
+
+            if(st.empty()){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(st.top());
+            }
+
+            st.push(i);
         }
-        return sum;
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    // Previous Greater
+    vector<int> prevgreater_Ele(vector<int>& arr){
+        vector<int> ans;
+        stack<int> st;
+
+        for(int i = 0; i < arr.size(); i++){
+            while(!st.empty() && arr[st.top()] <= arr[i]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(st.top());
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    // Next Smaller
+    vector<int> nextsmaller_Ele(vector<int>& arr){
+        stack<int> st;
+        vector<int> ans;
+
+        for(int i = arr.size()-1; i >= 0; i--){
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(st.top());
+            }
+
+            st.push(i);
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+
+    // Previous Smaller
+    vector<int> prevsmaler_Ele(vector<int>& arr){
+        vector<int> ans;
+        stack<int> st;
+
+        for(int i = 0; i < arr.size(); i++){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+
+            if(st.empty()){
+                ans.push_back(-1);
+            }
+            else{
+                ans.push_back(st.top());
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+
+    long long subarrayMax(vector<int>& arr){
+        long long total = 0;
+
+        vector<int> NGE = nextgreater_Ele(arr);
+        vector<int> PGE = prevgreater_Ele(arr);
+
+        for(int i = 0; i < arr.size(); i++){
+            long long left = i - PGE[i];
+            long long right = (NGE[i] == -1 ? arr.size() : NGE[i]) - i;
+
+            total += left * right * arr[i];
+        }
+
+        return total;
+    }
+
+    long long subarrayMin(vector<int>& arr){
+        long long total = 0;
+
+        vector<int> NSE = nextsmaller_Ele(arr);
+        vector<int> PSE = prevsmaler_Ele(arr);
+
+        for(int i = 0; i < arr.size(); i++){
+            long long left = i - PSE[i];
+            long long right = (NSE[i] == -1 ? arr.size() : NSE[i]) - i;
+
+            total += left * right * arr[i];
+        }
+
+        return total;
+    }
+
+    long long subArrayRanges(vector<int>& nums) {
+
+        long long maximum = subarrayMax(nums);
+        long long minimum = subarrayMin(nums);
+
+        return maximum - minimum;
     }
 };
