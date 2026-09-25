7cefa314c16a04d135c548bcae9be9cd91193177
Prashant Upadhyay <prashant.24scse1011301@galgotiasuniversity.ac.in>
2026-09-25 23:19:30 +0530

    Time: 0 ms (100%), Space: 20.7 MB (53.1%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-int recursion(int index, int jumps,vector<int>& nums){
-    if(index>=nums.size()-1) {
-        return jumps;
-    }
-    int mini_jumps = INT_MAX;
-    for(int i=1; i<=nums[index]; i++){
-        mini_jumps = min(mini_jumps,recursion(index+i,jumps+1,nums));
-    }
-    return mini_jumps;
-}
-
-int memoisation(int index,vector<int>& nums,vector<int>&dp){
-    if(index>=nums.size()-1) {
-        return 0;
-    }
-    if(dp[index]!=-1){
-        return dp[index];
-    }
-    int mini_jumps = INT_MAX;
-    for(int i=1; i<=nums[index]; i++){
-    int ans = memoisation(index+i, nums, dp);
-
-    if(ans != INT_MAX){
-        mini_jumps = min(mini_jumps, 1 + ans);
-    }
-  }
-    return dp[index]= mini_jumps;
-}
-
     int jump(vector<int>& nums) {
-        // return recursion(0,0,nums); // index , jumps
-      vector<int>dp(nums.size(),-1);
-        return memoisation(0,nums,dp);
+        int l = 0;
+        int r = 0;
+        long long jumps = 0;
+        while(r<nums.size()-1){
+            int farthest = 0;
+            for(int i=l; i<=r; i++){
+            farthest = max(farthest,i+nums[i]);
+            }
+            l = r+1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
     }
 };
